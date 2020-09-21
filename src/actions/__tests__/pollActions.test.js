a4b3a5d0c85b22a9e3d89201656033623b1fb799
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-09-21 20:48:09 +0200

    style: Make tests more readable

--- src/actions/__tests__/pollActions.test.js
+++ src/actions/__tests__/pollActions.test.js
@@ -3,27 +3,26 @@ import * as types from '../../constants/actionTypes';
 
 describe('poll actions', () => {
   it('should create an action to add a new poll', () => {
-    const optionOne = 'be a Jedi';
-    const optionTwo = 'be a Sith';
     const expectedAction = {
       type: types.NEW_POLL,
       payload: {
-        optionOne,
-        optionTwo
+        optionOne: 'be a Jedi',
+        optionTwo: 'be a Sith'
       }
     };
-    expect(actions.newPoll(optionOne, optionTwo)).toEqual(expectedAction);
+    expect(actions.newPoll(optionOne, optionTwo))
+      .toEqual(expectedAction);
   });
+
   it('should create an action to vote on a poll', () => {
-    const id = '13';
-    const option = 'optionTwo';
     const expectedAction = {
       type: types.TAKE_POLL,
       payload: {
-        id,
-        option
+        id: '13',
+        option: 'optionTwo'
       }
     };
-    expect(actions.takePoll(id, option)).toEqual(expectedAction);
+    expect(actions.takePoll(id, option))
+      .toEqual(expectedAction);
   });
 });
--- src/actions/__tests__/userActions.test.js
+++ src/actions/__tests__/userActions.test.js
@@ -3,31 +3,28 @@ import * as types from '../../constants/actionTypes';
 
 describe('user actions', () => {
   it('should create an action to log in', () => {
-    const username = 'admin';
-    const password = 'letMeIn!4!';
     const expectedAction = {
       type: types.LOGIN,
       payload: {
-        username,
-        password
+        username: 'admin',
+        password: 'letMeIn!4!'
       }
     };
-    expect(actions.login(username, password)).toEqual(expectedAction);
+    expect(actions.login(username, password))
+      .toEqual(expectedAction);
   });
+
   it('should create an action to register a new user', () => {
-    const username = 'admin';
-    const name = 'Adam Minuteman';
-    const avatar = 'male04';
-    const password = 'letMeIn!4!';
     const expectedAction = {
       type: types.REGISTER,
       payload: {
-        username,
-        name,
-        avatar,
-        password
+        username: 'admin',
+        name: 'Adam Minuteman',
+        avatar: 'male04',
+        password: 'letMeIn!4!'
       }
     };
-    expect(actions.register(username, name, avatar, password)).toEqual(expectedAction);
+    expect(actions.register(username, name, avatar, password))
+      .toEqual(expectedAction);
   });
 });
