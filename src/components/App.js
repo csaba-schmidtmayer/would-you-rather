a07f2f7c567a3f147253b125ecbdc817c44a9910
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-01 18:31:05 +0200

    feat: Add dummy pages

--- src/components/App.js
+++ src/components/App.js
@@ -3,19 +3,30 @@ import { connect } from 'react-redux';
 import { Switch, Route } from 'react-router-dom';
 import LoadingBar from 'react-redux-loading-bar';
 
+import Header from './Header';
+import Dashboard from '../pages/Dashboard';
+import Leaderboard from '../pages/Leaderboard';
+import NewPoll from '../pages/NewPoll';
 import Login from '../pages/Login';
 import Register from '../pages/Register';
 
 function App(props) {
   return (
     <div className="App">
+      {props.isLoggedIn ? <Header /> : null}
       <LoadingBar />
       <Switch>
-        <Route exact path="/" component={Login} />
+        <Route exact path="/" component={props.isLoggedIn ? Dashboard : Login} />
+        <Route path="/leaderboard" component={props.isLoggedIn ? Leaderboard : Login} />
+        <Route path="/add" component={props.isLoggedIn ? NewPoll : Login} />
         <Route path="/register" component={Register} />
       </Switch>
     </div>
   );
 }
 
-export default connect()(App);
+const mapStateToProps = ({ activeUser }) => ({
+  isLoggedIn: activeUser.activeUser === null ? false : true
+});
+
+export default connect(mapStateToProps)(App);
---
+++ src/pages/NewPoll.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class NewPoll extends React.Component {
+  render() {
+    return (
+      <div>
+        CREATE NEW POLL
+      </div>
+    );
+  }
+}
+
+export default NewPoll;
