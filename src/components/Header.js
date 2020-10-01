2ddc339acc944e38d9f4eae60726c0092e247c22
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-01 18:18:28 +0200

    feat: Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { Link } from 'react-router-dom';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.logout = this.logout.bind(this);
+  }
+
+  logout() {
+
+  }
+
+  render() {
+    return (
+      <div className="header">
+        <Link to="/">
+          Dashboard
+        </Link>
+        <Link to="/leaderboard">
+          Leaderboard
+        </Link>
+        <Link to="/add">
+          New poll
+        </Link>
+        <div className="user-interaction">
+          {/* Avatar placeholder */}
+          <ul>
+            <li>View profile</li>
+            <li onClick={this.logout}>Logout: {this.props.username}</li>
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = ({ activeUser }) => ({
+  username: activeUser.username,
+  name: activeUser.name,
+  avatar: activeUser.avatar
+});
+
+export default connect(mapStateToProps)(Header);
