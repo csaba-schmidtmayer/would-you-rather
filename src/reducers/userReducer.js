40ea1539c1c64dedd9dff372738a2a6caf59c1c9
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-04 21:17:31 +0200

    fix: Stop updating the store at registration

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,19 +1,8 @@
-import { REGISTER, POPULATE_USERS, CLEAR_STORE } from '../constants/actionTypes';
+import { POPULATE_USERS, CLEAR_STORE } from '../constants/actionTypes';
 
 const userReducer = (state = {}, action) => {
   const { type, payload } = action;
   switch(type) {
-    case REGISTER:
-      return ({
-        ...state,
-        [payload.username]: {
-          username: payload.username,
-          name: payload.name,
-          avatar: payload.avatar,
-          answers: {},
-          questions: []
-        }
-      });
     case POPULATE_USERS:
       const usersAssocArr = {};
       payload.users.forEach((user) => {
