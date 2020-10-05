2be261010ff7f7e601f83c4b9e8a080a33af8fcb
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-05 20:54:26 +0200

    feat: Update dashboard to list polls

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,13 +1,58 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import PollOverview from '../components/PollOverview';
 
 class Dashboard extends React.Component {
   render() {
     return (
       <div>
-        DASHBOARD
+        <div>
+          <p>Unanswered Polls</p>
+          <div>
+            {this.props.unansweredPolls.map((poll) => (
+              <PollOverview
+                key={poll.id}
+                id={poll.id}
+                optionOne={poll.optionOne}
+                optionTwo={poll.optionTwo}
+              />
+            ))}
+          </div>
+        </div>
+        <div>
+          <p>Answered Polls</p>
+          <div>
+            {this.props.answeredPolls.map((poll) => (
+              <PollOverview
+                key={poll.id}
+                id={poll.id}
+                optionOne={poll.optionOne}
+                optionTwo={poll.optionTwo}
+              />
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
 }
 
-export default Dashboard;
+const mapStateToProps = ({ polls, activeUser }) => {
+  const allPolls = Object.keys(polls).map((key) =>({
+    id: key,
+    optionOne: polls[key].optionOne.text,
+    optionTwo: polls[key].optionTwo.text,
+    answer: activeUser.answers[key] !== undefined
+      ? activeUser.answers[key]
+      : null
+  }));
+  const unansweredPolls = allPolls.filter((poll) => (poll.answer === null));
+  const answeredPolls = allPolls.filter((poll) => (poll.answer !== null));
+  return {
+    unansweredPolls,
+    answeredPolls
+  };
+};
+
+export default connect(mapStateToProps)(Dashboard);
