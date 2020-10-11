ef9df60a2490139177db5b9d4955b424157ddd38
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-11 15:29:06 +0200

    refactor: Refactor navbar-link into a component

--- src/components/header/Navbar.js
+++ src/components/header/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
+import NavbarLink from './NavbarLink';
 import home from '../../svg/home.svg';
 import trophy from '../../svg/trophy.svg';
 import pencil from '../../svg/pencil.svg';
@@ -8,38 +8,35 @@ import pencil from '../../svg/pencil.svg';
 class Navbar extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      links: [
+        {
+          linkTo: '/',
+          icon: home
+        },
+        {
+          linkTo: '/leaderboard',
+          icon: trophy
+        },
+        {
+          linkTo: '/add',
+          icon: pencil
+        },
+      ]
+    }
   }
 
   render() {
     return (
       <nav className="navbar">
-        <Link to="/">
-          <div className="navbar-link">
-            <img
-              className="main-header-icon"
-              src={home}
-            />
-            <div className="navbar-link-active" />
-          </div>
-        </Link>
-        <Link to="/leaderboard">
-          <div className="navbar-link">
-            <img
-              className="main-header-icon"
-              src={trophy}
-            />
-            <div className="navbar-link-active" />
-          </div>
-        </Link>
-        <Link to="/add">
-          <div className="navbar-link">
-            <img
-              className="main-header-icon"
-              src={pencil}
-            />
-            <div className="navbar-link-active" />
-          </div>
-        </Link>
+        {this.state.links.map((link) => (
+          <NavbarLink
+            key={link.linkTo}
+            linkTo={link.linkTo}
+            icon={link.icon}
+          />
+        ))}
       </nav>
     );
   }
---
+++ src/components/header/NavbarLink.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const NavbarLink = (props) => (
+  <Link to={props.linkTo}>
+    <div className="navbar-link">
+      <img
+        className="main-header-icon"
+        src={props.icon}
+      />
+      <div className="navbar-link-active" />
+    </div>
+  </Link>
+);
+
+export default NavbarLink;
