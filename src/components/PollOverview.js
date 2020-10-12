1f4e590e957671fb87f84139ad573db6e4e3db31
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-12 21:11:28 +0200

    feat: Style poll overviews

--- src/components/PollOverview.js
+++ src/components/PollOverview.js
@@ -3,10 +3,15 @@ import { Link } from 'react-router-dom';
 
 const PollOverview = (props) => (
   <Link to={`/polls/${props.id}`}>
-    <div>
-      <p>Would you rather...?</p>
-      <p>{props.optionOne}</p>
-      <p>{props.optionTwo}</p>
+    <div className="poll-overview">
+      <div className="poll-overview-header">
+        Would you rather...?
+      </div>
+      <div className="poll-overview-options">
+        <span>{props.optionOne}</span>
+        <span className="poll-overview-divider">or</span>
+        <span>{props.optionTwo}</span>
+      </div>
     </div>
   </Link>
 );
