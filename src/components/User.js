ad3a1311ecc7af1118951757a7ebd02681f8f59e
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-17 20:45:12 +0200

    feat: Style user statistics panel

--- src/components/User.js
+++ src/components/User.js
@@ -1,17 +1,63 @@
 import React from 'react';
 
-import avatars from '../svg/avatars';
+import UserHeader from './UserHeader';
 
-const User = (props) => (
-  <div>
-    <p>{props.place}</p>
-    <img src={avatars[props.avatar]} width="100px" height="100px" />
-    <p>{props.name}</p>
-    <p>{props.username}</p>
-    <p>{`Submitted ${props.numOfPolls === 0 ? 'no polls yet' : props.numOfPolls + ' poll'}${props.numOfPolls > 1 ? 's' : ''}: ${props.pollPoints} p.`}</p>
-    <p>{`Answered ${props.numOfAnswers === 0 ? 'no polls yet' : props.numOfAnswers + ' poll'}${props.numOfAnswers > 1 ? 's' : ''}: ${props.answerPoints} p.`}</p>
-    <p>Total: {props.pollPoints + props.answerPoints}</p>
-  </div>
-);
+const mapOutPoints = (categories) => {
+  const withTotal = [
+    ...categories,
+    {
+      text: 'Total',
+      point: categories.reduce((acc, curr) => (acc + curr.point), 0)
+    }
+  ];
+  return withTotal.map((category) => (
+    <div
+      className={`stat-row${category.text === 'Total' ? ' total-row' : ''}`}
+      key={category.text.toLowerCase()}
+    >
+      <span>
+        {category.text === 'Total'
+          ? `${category.text}: `
+          : `${category.text} ${category.number === 0 ? 'no polls yet' : category.number + ' poll'}${category.number > 1 ? 's' : ''}: `
+        }
+        </span>
+      <span>
+        {category.point}
+      </span>
+      <span>
+        {` ${category.point === 1 ? 'point' : 'points'}`}
+      </span>
+    </div>
+  ))
+};
+
+const User = (props) => {
+  const categories = [
+    {
+      text: 'Submitted',
+      number: props.numOfPolls,
+      point: props.pollPoints
+    },
+    {
+      text: 'Answered',
+      number: props.numOfAnswers,
+      point: props.answerPoints
+    }
+  ];
+  return (
+    <div className="user">
+      <UserHeader
+        username={props.username}
+        additionalInfo={`User rank: ${props.place}`}
+      />
+      <div className="stat-header">
+        User statistics
+      </div>
+      <div className="stat-rows">
+        {mapOutPoints(categories)}
+      </div>
+    </div>
+  );
+};
 
 export default User;
