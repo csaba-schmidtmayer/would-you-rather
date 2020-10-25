1f30919fdc6d7d3ac1fecd39b88c7d2aa6d13403
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-25 08:22:33 +0100

    refactor: Refactor css into sass

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './components/App';
 import configureStore from './utils/configureStore';
 import * as serviceWorker from './serviceWorker';
-import './index.css';
+import './css/main.scss';
 
 const store = configureStore();
 
