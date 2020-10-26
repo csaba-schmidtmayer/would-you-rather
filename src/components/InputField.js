37353ff72f6c12b37104f4e8ee40a1671e329622
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-26 21:45:06 +0100

    feat: Add icon handling to input fields

--- src/components/InputField.js
+++ src/components/InputField.js
@@ -1,32 +1,44 @@
 import React, { useState } from 'react';
 
 const InputField = (props) => {
-    const [ inputRef, setInputRef ] = useState(null);
-    const [ hasFocus, setHasFocus ] = useState(false);
-    return (
-    <div className={`input-wrapper${hasFocus ? ' input-active' : ''}`}>
-      <input
-        name={props.name}
-        type={props.type}
-        value={props.value}
-        onChange={props.onChange}
-        onFocus={() => {setHasFocus(true)}}
-        onBlur={() => {setHasFocus(false)}}
-        ref={(input) => {setInputRef(input)}}
-      />
-      <span
-        className={`input-placeholder${hasFocus ? ' input-active' : ''}${props.value !== '' ? ' input-filled' : ''}`}
-        onClick={() => {inputRef.focus()}}>
-        {props.placeholder}
-      </span>
+  const [ inputRef, setInputRef ] = useState(null);
+  const [ hasFocus, setHasFocus ] = useState(false);
+  return (
+    <div className={`input-field${hasFocus ? ' input-active' : ''}`}>
       {
-        props.warning === undefined
-          ? null
-          : (
-            <span className="input-warning">
-              {props.warning}
-            </span>
-          )
+        props.leadIconComp !== undefined
+          ? props.leadIconComp
+          : null
+      }
+      <div className={`input-wrapper${hasFocus ? ' input-active' : ''}`}>
+        <input
+          name={props.name}
+          type={props.type}
+          value={props.value}
+          onChange={props.onChange}
+          onFocus={() => {setHasFocus(true)}}
+          onBlur={() => {setHasFocus(false)}}
+          ref={(input) => {setInputRef(input)}}
+        />
+        <span
+          className={`input-placeholder${hasFocus ? ' input-active' : ''}${props.value !== '' ? ' input-filled' : ''}`}
+          onClick={() => {inputRef.focus()}}>
+          {props.placeholder}
+        </span>
+        {
+          props.warning === undefined
+            ? null
+            : (
+              <span className="input-warning">
+                {props.warning}
+              </span>
+            )
+        }
+      </div>
+      {
+        props.trailIconComp !== undefined
+          ? props.trailIconComp
+          : null
       }
     </div>
   );
