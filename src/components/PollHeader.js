5b1d1ceff777f2e1f084ca1d34a6658a785cb619
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-27 17:30:23 +0100

    refactor: Add custom text prop to PollHeader

--- src/components/PollHeader.js
+++ src/components/PollHeader.js
@@ -2,7 +2,11 @@ import React from 'react';
 
 const PollHeader = (props) => (
   <div className="poll-header">
-    Would you rather...?
+    {
+      props.text !== undefined
+        ? props.text
+        : 'Would you rather...?'
+    }  
   </div>
 );
 
