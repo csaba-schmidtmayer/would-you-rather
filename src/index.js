535e9d8c851a14141d3ca65c82595d61dbfba1a7
Csaba Schmidtmayer <cs.schmidtmayer@gmail.com>
2020-10-28 16:13:06 +0100

    chore: Add base folder for web deployment

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const store = configureStore();
 
 ReactDOM.render(
   <Provider store={store}>
-    <Router>
+    <Router basename="/would-you-rather">
       <App />
     </Router>
   </Provider>,
